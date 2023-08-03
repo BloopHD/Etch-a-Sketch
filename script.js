c8a9d7f734757d8d6e73d6700c4dafabc2972d87
Mickey Carlton <mrobcarlton@gmail.com>
2023-08-02 21:23:27 -0400

    Added some options, and optimized some code.

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 let gridContainer = document.getElementById("gridContainer");
 let sizeButton = document.getElementById("buttonSize");
 let refreshButton = document.getElementById("buttonRefresh");
+let rainbowButton = document.getElementById("buttonRainbow");
+let coloringButton = document.getElementById("buttonColoring");
+let eraserButton = document.getElementById("buttonEraser");
 
-let gridSize = 16;
+let gridSize = 25;
+let rainbowMode = false;
+let eraserMode = false;
+let color = "rgb(64,64,64)";
 
-// sets up buttons
-sizeButton.addEventListener('click', getSize);
-refreshButton.addEventListener('click', createGrid);
-
+setUpButtons();
+preventDrag();
 createGrid();
 
 // Gets size of grid from user.
 function getSize() {
     do {
 
-        gridSize = prompt("Enter grid size:");
+        gridSize = prompt("Enter grid size (1-100):");
 
-    } while (gridSize < 0 || gridSize > 100);
+    } while (gridSize < 0 || gridSize > 100 || isNaN(parseInt(gridSize)));
         
     createGrid();
 }
 
 // Draws on the square.
 function drawOn(e) {
-    e.target.classList.add("hovered");
+
+    if (eraserMode) {
+        color = "rgb(255,255,255)"
+    } else if (rainbowMode) {
+        color = randomColor();
+    } else {
+        color = "rgb(64,64,64)"
+    }
+
+    e.target.style.backgroundColor = color;
+
 }
 
 // Creates grid to size - gridSize.
@@ -41,13 +55,17 @@ function createGrid() {
             const col = document.createElement('div');
             col.className = "col"
 
-            col.addEventListener('mouseover', drawOn);
+            //col.addEventListener('mouseover', drawOn);
 
             // Use this to have it down on mousedown while moving mouseover?
             // doesn't work...
-            // col.addEventListener('mouseover', () => {
-            //     col.addEventListener('mouseover', drawOn)
-            // });
+            gridContainer.addEventListener('mousedown', () => {
+                col.addEventListener('mouseover', drawOn)
+            });
+
+            window.addEventListener('mouseup', ()=> {
+                col.removeEventListener('mouseover', drawOn)
+            })
 
             row.appendChild(col);
         }   
@@ -57,4 +75,51 @@ function createGrid() {
 // Destroys grid to make room for new grid.
 function destroyGrid() {
     gridContainer.innerHTML = '';
+}
+
+function randomColor() {
+
+    let r = randomColorNumber();
+    let g = randomColorNumber();
+    let b = randomColorNumber();
+
+    return "rgb("+[r,g,b]+")";
+}
+
+function randomColorNumber() {
+
+    return Math.floor(Math.random() * 255) + 1  // between 0 - 256
+}
+
+function setUpButtons() {
+
+    sizeButton.addEventListener('click', getSize);
+    refreshButton.addEventListener('click', createGrid);
+    rainbowButton.addEventListener('click', () => { 
+        rainbowMode = true; 
+        eraserMode = false 
+    });
+    coloringButton.addEventListener('click', () => { 
+        rainbowMode = false; 
+        eraserMode = false;
+    });
+    eraserButton.addEventListener('click', () => { 
+        rainbowMode = false; 
+        eraserMode = true; 
+    });
+}
+
+function setColor() {
+    color = "rgb(64,64,64)";
+}
+
+function preventDrag() {
+
+    document.body.addEventListener('dragstart', (e) => {
+        e.preventDefault();
+    })
+    
+    document.body.addEventListener('drop', (e) => {
+        e.preventDefault();
+    })
 }
